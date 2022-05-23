3922aefccb45f1e5029f3daacb76fc3a2037e97a
igor.petrenko <igor.petrenko@xenoss.io>
2022-05-23 16:49:15 +0300

    fix: version

--- version.sh
+++ version.sh
@@ -6,7 +6,7 @@ BUILD_COUNTER=$1
 PROJECT_NAME=$2
 BRANCH_NAME=$3
 
-VERSION_XENOSS=$(grep -oP 'project\.version\>\K[^<]*' pom.xml)
+VERSION_XENOSS=$(grep -oP 'clickhouse\.version\>\K[^<]*' pom.xml)
 
 if [ "$BRANCH_NAME" == "master" ] || [ "$BRANCH_NAME" == "" ] || [ "$BRANCH_NAME" == "refs/heads/master" ]; then
   VERSION_BRANCH=""
@@ -22,7 +22,7 @@ set +x
 echo "##teamcity[setParameter name='oap.project.name' value='${PROJECT_NAME,,}']"
 
 #maven master
-echo "##teamcity[setParameter name='${PROJECT_NAME,,}.project.version' value='${VERSION_XENOSS}']"
+echo "##teamcity[setParameter name='oap.project.${PROJECT_NAME,,}.version' value='${VERSION_XENOSS}']"
 
 #maven branch
 echo "##teamcity[setParameter name='oap.project.version.branch' value='${VERSION_XENOSS}${VERSION_BRANCH}${MAVEN_BUILD_COUNTER}']"
